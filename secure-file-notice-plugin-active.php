0e05eee97caab3b3a819214ce81c743e8e4b185d
Vitus Schuhwerk <vitus.schuhwerk@gmail.com>
2021-02-08 21:57:25 +0100

    fix: avoid admin_notice error

--- secure-file-notice-plugin-active.php
+++ secure-file-notice-plugin-active.php
@@ -4,20 +4,22 @@
  * It is loaded in wp-config.php and notices an admin, if the plugin is not active.
  */
 
-add_action(
-	'admin_notices',
-	function() {
-		if ( is_plugin_active( 'secure-file/secure-file.php' ) ) {
-			return;
+if ( function_exists( 'add_action' ) ) {
+	add_action(
+		'admin_notices',
+		function() {
+			if ( is_plugin_active( 'secure-file/secure-file.php' ) ) {
+				return;
+			}
+
+			$message = __(
+				'This installation includes the secure-file plugin (probably via. wp-config). ' .
+				'Make sure the plugin network-activated, odd things may happen otherwise ;)',
+				'sfile'
+			);
+			$class = 'notice notice-warning';
+
+			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 		}
-
-		$message = __(
-			'This installation includes the secure-file plugin (probably via. wp-config). ' .
-			'Make sure the plugin network-activated, odd things may happen otherwise ;)',
-			'sfile'
-		);
-		$class = 'notice notice-warning';
-
-		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-	}
-);
+	);
+}
